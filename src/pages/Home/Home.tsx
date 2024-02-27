e233c52bf3e3ae01f5666661eed474e570b05a8f
Samuel <sam404kim@gmail.com>
2024-02-26 23:42:10 -0800

    adjusted padding

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <section
       className={`
-        flex flex-col items-center min-h-[100px] h-[95vh] z-[-100] relative w-[100%] mt-[47px] pt-[12rem]
+        flex flex-col items-center min-h-[100px] h-[95vh] z-[-100] relative w-[100%] mt-[47px] pt-[13rem]
         md:min-h-[1000px]
       `}
     >
